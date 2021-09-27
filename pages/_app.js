91f23039b9855e9e8af9c271017806c11d2e0248
Veronika Martinovich <veranika.mart@gmail.com>
2021-09-27 23:49:16 +0300

    feat: add loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import NextNprogress from 'nextjs-progressbar';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <NextNprogress
+      color="yellow"
+      startPosition={0.3}
+      stopDelayMs={200}
+      height={3}
+      showOnShallow={true}
+    />
       <Component {...pageProps} />
       <style jsx global>
       {`
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,10 @@
 import Router from 'next/router';
 import {MainLayout} from "../../components/MainLayout";
 
-const About = () => {
+export default function About ({title}) {
   return (
   <MainLayout title="About">
-    <h1>About</h1>
+    <h1>{title}</h1>
     <button onClick={() => {
       Router.push('/about/author')
     }}>Go to author</button>
@@ -12,4 +12,11 @@ const About = () => {
   )
 }
 
-export default About
+About.getInitialProps = async () => {
+  const response = await fetch('http://localhost:4200/about');
+  const data = await response.json();
+
+  return {
+    title: data.title
+  }
+}
