5dd9fbedd4d93b2d40df4a1f7b2da115180c8401
Veronika Martinovich <veranika.mart@gmail.com>
2021-09-27 21:51:52 +0300

    feat: get posts on posts page

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import {MainLayout} from "../components/MainLayout";
 
 export default function Posts () {
+  const [posts, setPosts] = useState() ;
+
+  useEffect(() => {
+    async function getPosts() {
+      const response = await fetch('http://localhost:4200/posts');
+      console.log(response)
+      const json = await response.json();
+      console.log(json)
+      setPosts(json);
+    };
+    getPosts();
+  }, [])
+
   return (
     <MainLayout title="Posts">
       <h1>Posts</h1>
+     {posts && ( 
+      <ul>
+        {posts.map((item) => (
+          <li key={item.id}>
+            {item.title}
+          </li>
+        ))}
+      </ul>
+      )}
     </MainLayout>
   )
 }
