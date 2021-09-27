08a8e8d69c8b7f6bbedc007a54f7280e3f976371
Veronika Martinovich <veranika.mart@gmail.com>
2021-09-27 23:23:57 +0300

    feat: add logic for client fetching

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,18 +1,50 @@
+import {useState, useEffect} from 'react';
 import {useRouter} from  'next/router';
 import {MainLayout} from "../../components/MainLayout";
+import Link from 'next/link';
 
-export default function Post ({post}) {
+export default function Post ({post: serverPost}) {
+  const [post, setPost] = useState(serverPost);
   const router = useRouter();
+
+  useEffect(()=> {
+    async function getPost () {
+      const response = await fetch(`http://localhost:4200/posts/${router.query.id}`);
+      const data = await response.json();
+      setPost(data);
+    }
+
+    if (!serverPost) {
+      getPost();
+    }
+  }, []);
+
+  if (!post) {
+    return (
+      <p>Loading ...</p>
+    )
+  }
 
   return (
     <MainLayout title="Posts">
-      <h1>{router.query.id}</h1>
       <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <Link href="/posts">
+        <a>
+          See all posts
+        </a>
+      </Link>
     </MainLayout>
   )
 }
 
-Post.getInitialProps =  async ({query}) => {
+Post.getInitialProps =  async ({query, req}) => {
+  if (!req) {
+    return {
+      post: null
+    }
+  }
+
   const response = await fetch(`http://localhost:4200/posts/${query.id}`);
   const post = await response.json();
 
